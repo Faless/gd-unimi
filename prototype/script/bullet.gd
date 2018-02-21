9842309262feddf41bdb3ebc1b519615295c3069
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2018-02-21 18:33:04 +0100

    Add missing bullet scene, make it explode

--- prototype/script/bullet.gd
+++ prototype/script/bullet.gd
@@ -3,20 +3,36 @@ extends RigidBody2D
 export var bullet_speed = 100
 
 var _update_time = 0.0
+var _exploded = false
 
 func _ready():
 	if Network.is_server():
 		Network.connect("network_tick", self, "send_pos_update")
-	pass
+		$Timer.start()
 
 func send_pos_update():
+	if _exploded:
+		return
 	rpc_unreliable("update_pos", OS.get_unix_time(), global_position)
 
 slave func update_pos(time, pos):
-	if _update_time > time:
+	if _exploded or _update_time > time:
 		return
 	_update_time = time
 	global_position = pos
 
-func _on_Timer_timeout():
+sync func explode(pos):
+	_exploded = true
+	global_position = pos
+	collision_layer = 0
+	collision_mask = 0
+	mode = MODE_STATIC
+	$AnimationPlayer.play("explode")
+	yield($AnimationPlayer, "animation_finished")
 	call_deferred("queue_free")
+
+func _on_Timer_timeout():
+	explode(global_position)
+
+func _on_Bullet_body_entered( body ):
+	rpc("explode", global_position)
