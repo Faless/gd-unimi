40122f182c94c77c2d2a4aab3d91f73c882a7d14
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2018-02-22 20:20:32 +0100

    Main now start local matches and report erros

--- prototype/script/main.gd
+++ prototype/script/main.gd
@@ -2,6 +2,8 @@ extends Control
 
 func _ready():
 	$Panel/VBoxContainer/Local.grab_focus()
+	if Network.is_error:
+		_show_error()
 
 func _on_Local_pressed():
 	$Local.show_modal(true)
@@ -12,21 +14,30 @@ func _on_online_pressed():
 	$Online.get_ok().grab_focus()
 
 func _on_local_start():
-	print("Start")
+	get_tree().change_scene("res://scene/world.tscn")
 
 func _on_local_cancel():
 	$Panel/VBoxContainer/Local.grab_focus()
 
 func _on_online_start():
 	if $Online/Control/VBoxContainer/HBoxContainer/CheckButton.pressed:
-		Network.start_server()
+		if not Network.start_server():
+			_show_error()
+			return
 		get_tree().change_scene("res://scene/world.tscn")
 	else:
 		var ip = $Online/Control/VBoxContainer/Client/LineEdit.text
 		if not ip.is_valid_ip_address():
 			ip = "127.0.0.1"
-		Network.start_client(ip)
+		if not Network.start_client(ip):
+			_show_error()
+			return
 		get_tree().change_scene("res://scene/world.tscn")
 
 func _on_online_cancel():
 	$Panel/VBoxContainer/Online.grab_focus()
+
+func _show_error():
+	Network.is_error = false
+	$Error.show_modal(true)
+	$Error.get_ok().grab_focus()
