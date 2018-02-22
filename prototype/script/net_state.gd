270b0d846269dd62d30c0e68b6687750aa6230cb
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2018-02-22 20:19:34 +0100

    Is online and error messages in net state

--- prototype/script/net_state.gd
+++ prototype/script/net_state.gd
@@ -6,8 +6,8 @@ signal delete_player(player_id)
 signal network_tick()
 
 const NETWORK_TICK = 0.0166
-var _players = []
 var _net_timer = 0.0
+var is_error = false
 
 func _ready():
 	get_tree().connect("network_peer_connected", self, "_peer_connected")
@@ -23,31 +23,36 @@ func _physics_process(delta):
 
 func start_server():
 	var host = NetworkedMultiplayerENet.new()
-	host.create_server(6969)
+	if host.create_server(6969) != OK:
+		_network_failed()
+		return false
 	get_tree().network_peer = host
+	return true
 
 func start_client(ip):
 	var host = NetworkedMultiplayerENet.new()
-	host.create_client(ip, 6969)
+	if host.create_client(ip, 6969) != OK:
+		_network_failed()
+		return false
 	get_tree().network_peer = host
+	return true
 
 func _peer_connected(peer_id):
-	_players.append(peer_id)
 	emit_signal("spawn_player", peer_id)
 
+func is_online():
+	return get_tree().network_peer != null
+
 func is_server():
-	return get_tree().network_peer == null or get_tree().is_network_server()
+	return not is_online() or get_tree().is_network_server()
 
 func get_peer_id():
 	return 1 if get_tree().network_peer == null else get_tree().get_network_unique_id()
 
-slave func add_player(peer_id):
-	print("add player", peer_id)
-
 func _peer_disconnected(peer_id):
-	_players.erase(peer_id)
 	emit_signal("delete_player", peer_id)
 
 func _network_failed():
 	get_tree().network_peer = null
+	is_error = true
 	emit_signal("network_disconnected")
